7d077ec0fd27a8f8e5be0751b558bbaa5d6ed89f
Gil Kléber <gilkleber@gmail.com>
2026-05-30 11:46:46 +0000

    File changes

--- src/api/base44Client.js
+++ src/api/base44Client.js
@@ -12,34 +12,40 @@ const rawClient = createClient({
   appBaseUrl
 });
 
-export const base44 = { ...rawClient };
+export const base44 = new Proxy(rawClient, {
+  get(target, prop) {
+    if (prop === 'entities') {
+      return new Proxy(target.entities, {
+        get(entTarget, entityName) {
+          const entity = entTarget[entityName];
+          if (!entity || typeof entity !== 'object') return entity;
 
-base44.entities = new Proxy(rawClient.entities, {
-  get(target, entityName) {
-    const entity = target[entityName];
-    if (!entity || typeof entity !== 'object') return entity;
-
-    return new Proxy(entity, {
-      get(entTarget, propName) {
-        const origMethod = entTarget[propName];
-        if (propName === 'create') {
-          return async function(data) {
-             if (window.__BASE44_FAMILY_ID) {
-                data.family_id = window.__BASE44_FAMILY_ID;
-             }
-             return origMethod.call(entTarget, data);
-          }
+          return new Proxy(entity, {
+            get(innerTarget, innerProp) {
+              const origMethod = innerTarget[innerProp];
+              if (innerProp === 'create') {
+                return async function(data) {
+                   if (window.__BASE44_FAMILY_ID) {
+                      data.family_id = window.__BASE44_FAMILY_ID;
+                   }
+                   return origMethod.call(innerTarget, data);
+                }
+              }
+              if (innerProp === 'bulkCreate') {
+                return async function(dataArray) {
+                   if (window.__BASE44_FAMILY_ID) {
+                      dataArray.forEach(d => d.family_id = window.__BASE44_FAMILY_ID);
+                   }
+                   return origMethod.call(innerTarget, dataArray);
+                }
+              }
+              return origMethod;
+            }
+          });
         }
-        if (propName === 'bulkCreate') {
-          return async function(dataArray) {
-             if (window.__BASE44_FAMILY_ID) {
-                dataArray.forEach(d => d.family_id = window.__BASE44_FAMILY_ID);
-             }
-             return origMethod.call(entTarget, dataArray);
-          }
-        }
-        return origMethod;
-      }
-    });
+      });
+    }
+    const val = target[prop];
+    return typeof val === 'function' ? val.bind(target) : val;
   }
 });
